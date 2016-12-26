f90c0b507f836248ea655dcc6928efb3b07c4d60
wangpin34 <270280522@qq.com>
2016-12-26 14:32:25 +0800

    release v0.0.1

--- src/updater.js
+++ src/updater.js
@@ -1,15 +1,36 @@
-const {autoUpdater} = require('electron')
+const { autoUpdater } = require('electron-auto-updater')
 
-const target = ''
+
+const target = 'onshape-download.develar.org'
 
 const start = () => {
-	autoUpdater.setFeedURI(target)
+	autoUpdater.setFeedURL()
 
-	autoUpdater.checkForUpdates()
+	autoUpdater.checkForUpdates().then((result) => {
+		console.log('check for update result: ' + result) 
+	})
+
+	autoUpdater.on('checking-for-update', () => {
+		console.log('checking-for-update')
+	})
+
+	autoUpdater.on('update-available', () => {
+		console.log('update-available')
+	})
+
+	autoUpdater.on('update-not-available', () => {
+		console.warn('update-not-available')
+	})
 
 	autoUpdater.on('update-downloaded', () => {
 		autoUpdater.quitAndInstall()
 	})
+
+	autoUpdater.on('error', (error) => {
+		console.error('error happened')
+		console.error(error)
+	})
+
 }
 
 module.exports = {
